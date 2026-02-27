4b256ff63daf617d87d700bd673e47009528ac2a
mbj <blaaber@live.dk>
2026-02-27 14:30:50 +0100

    cleanup

--- setup-scripts/create-iso-from-proxmox.sh
+++
@@ -1,9 +0,0 @@
-echo "##### Downloading and unpacking Proxmox VE ISO #####"
-
-echo "Deleting existing ISO and mount directories..."
-sudo rm -f -r ~/proxmox-iso
-rm -f ~/Downloads/proxmox-ve_9.1-1.iso
-
-echo "Downloading Proxmox VE ISO..."
-curl --create-dirs -O --output-dir ~/Downloads https://enterprise.proxmox.com/iso/proxmox-ve_9.1-1.iso
-
